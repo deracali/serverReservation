8f94df04a3fb233a3575983dd83a89d304916fe2
deracali <chideracalistus00@gmail.com>
2024-10-21 16:33:59 -0700

    route changes

--- Routes/hotelBookRoute.js
+++ Routes/hotelBookRoute.js
@@ -10,9 +10,9 @@ const hotelBookRouter = express.Router();
 
 // Define routes with unique paths
 hotelBookRouter.post('', createHotelBooking); // Endpoint to create a booking
-hotelBookRouter.get('/user/:userId', getUserBookings); // Endpoint to get user bookings
-hotelBookRouter.get('/', getAllBookings); // Endpoint to get all bookings
-hotelBookRouter.get('/:id', getHotelBookingById); // Endpoint to get a booking by ID
+hotelBookRouter.get('', getUserBookings); // Endpoint to get user bookings
+hotelBookRouter.get('', getAllBookings); // Endpoint to get all bookings
+hotelBookRouter.get('', getHotelBookingById); // Endpoint to get a booking by ID
 
 // Export the router
 module.exports = {createHotelBooking,

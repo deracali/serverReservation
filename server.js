02d5f82ddd7327ee027bc032a74ce2ae732e7365
deracali <chideracalistus00@gmail.com>
2024-10-21 14:04:57 -0700

    added a port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const hotelBookRouter = require('./Routes/hotelBookRoute.js')
 const visaAppRouter = require('./Routes/visaAppRoute.js')
 const flightRouter = require('./Routes/flightBooking.js')
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000;
 connectDB()
 
 
